c44c3a9ef467f348e2134115dc0fc87c10c74eff
Brandon Sturgeon <sturgeonb4@gmail.com>
2019-08-14 00:48:09 -0400

    Add sendgrid credentials

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  user_name: Rails.application.credentials.sendgrid[:username],
+  password: Rails.application.credentials.sendgrid[:password],
+  domain: 'cfcservers.org',
+  address: 'smtp.sendgrid.net',
+  port: 465,
+  authentication: :plain,
+  enable_starttls_auto: true
+}
