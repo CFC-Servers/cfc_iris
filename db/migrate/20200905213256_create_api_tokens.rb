3ec63110d0e855cf88a3c115504add72bef7446e
Brandon Sturgeon <sturgeonb4@gmail.com>
2020-09-05 19:08:44 -0400

    binary -> string

--- db/migrate/20200905213256_create_api_tokens.rb
+++ db/migrate/20200905213256_create_api_tokens.rb
@@ -1,7 +1,7 @@
 class CreateApiTokens < ActiveRecord::Migration[5.2]
   def change
     create_table :api_tokens do |t|
-      t.binary :key, null: false, limit: 16
+      t.string :key, null: false
       t.boolean :active, default: true
       t.string :description
 
