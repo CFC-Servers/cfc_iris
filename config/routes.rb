d9f66a96e505434477105926b69425e2b83840e5
Brandon Sturgeon <sturgeonb4@gmail.com>
2020-09-05 21:05:11 -0400

    Add steam ids route

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@
 Rails.application.routes.draw do
   scope 'api' do
     get 'callbacks/discord', to: 'callbacks#receive_discord_callback'
-    get 'users/find', to: 'users#find_user'
-    post 'ranks/bulk_update', to: 'ranks#update_ranks'
     get 'users/:id', to: 'users#get'
+    get 'users/find', to: 'users#find_user'
+    get 'users/steam_ids', to: 'users#steam_ids'
+    post 'ranks/bulk_update', to: 'ranks#update_ranks'
   end
 end
