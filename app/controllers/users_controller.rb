fd577eab1bb87b06653bf97437703a1b32dc3999
Brandon Sturgeon <sturgeonb4@gmail.com>
2020-09-05 21:05:11 -0400

    Add steam ids route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,19 +3,26 @@
 class UsersController < AuthenticatedController
   def find_user
     platforms = params.permit(:steam, :discord)
+
     platforms.each_pair do |platform, identifier|
       identity = Identity.find_by(platform: platform, identifier: identifier)
+
       if identity
-        return render json: identity.user.as_json(include: [:identities, :ranks])
+        return render json: identity.user.as_json(include: %i[identities ranks])
       end
     end
-    render json: {error: 'User not found'}, status: 404
+
+    render json: { error: 'User not found' }, status: 404
   end
 
   def get
     user = User.find_by(id: params[:id])
-    return render plain: 'User not found', status: 404 unless user
+    return render json: { error: 'User not found' }, status: 404 unless user
 
-    render json: user.as_json(include: [:identities, :ranks])
+    render json: user.as_json(include: %i[identities ranks])
+  end
+
+  def steam_ids
+    render json: Identity.where(platform: 'steam').all.pluck(:identifier)
   end
 end
