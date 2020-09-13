4cebcfdbe89c170f609882d1e32aac8f03c66666
Brandon Sturgeon <sturgeonb4@gmail.com>
2020-09-12 23:12:04 -0400

    Cleanup

--- app/controllers/authenticated_controller.rb
+++ app/controllers/authenticated_controller.rb
@@ -7,17 +7,7 @@ class AuthenticatedController < ApplicationController
 
   def authenticate
     token = request.headers['Authorization']&.split&.last
-    Rails.logger.info('Received request with following headers:')
-    Rails.logger.info(request.headers['Authorization'])
-    Rails.logger.info(request.headers[:Authorization])
-    Rails.logger.info(request.authorization)
-    Rails.logger.info(request.env['Authorization'])
-    Rails.logger.info('With authorization token of:')
-    Rails.logger.info(token)
-    Rails.logger.info('')
-
     existing = ApiToken.find_by(key: token)
-    Rails.logger.info(existing)
 
     head :unauthorized unless existing
   end
