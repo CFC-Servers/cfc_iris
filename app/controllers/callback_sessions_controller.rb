dcdd67e6a4106a6b6f8cdbfc0a64f840d1c6ea26
Brandon Sturgeon <sturgeonb4@gmail.com>
2020-09-12 23:12:04 -0400

    Cleanup

--- app/controllers/callback_sessions_controller.rb
+++ app/controllers/callback_sessions_controller.rb
@@ -3,7 +3,7 @@
 class CallbackSessionsController < ApplicationController
   def results
     session = CallbackSession.find_by(uuid: params['uuid'])
-    render head: :not_found unless session
+    head :not_found unless session
 
     session.update(active: false)
     render json: { results: session.transform_results }
