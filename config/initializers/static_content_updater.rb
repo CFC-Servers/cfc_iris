71dbd04dffec0ec67738bd1c59edb29a288742ee
Brandon Sturgeon <brandon@brandonsturgeon.com>
2020-11-11 03:07:44 -0800

    Add frontend content

---
+++ config/initializers/static_content_updater.rb
@@ -0,0 +1,18 @@
+repo_link = git@github.com:CFC-Servers/cfc_iris_frontend.git
+
+current_frontend_version = `cat public/frontend/VERSION`
+latest_frontend_version = `git ls-remote --refs --sort='version:refname' --tags #{repo_link} | cut -d/ -f3-|tail -n1`
+
+Rails.logger.debug "Current frontend content version: '#{current_frontend_version}'"
+Rails.logger.debug "Latest frontend content version: '#{latest_frontend_version}'"
+
+if current_frontend_version != latest_frontend_version
+  Rails.logger.info "Current frontend content is out of date, getting the latest..."
+  `rm -rf public/frontend/*`
+
+  url = "https://github.com/CFC-Servers/cfc_iris_frontend/releases/latest/download/built-site.tar.bz2"
+  `curl -LO #{url} public/frontend/`
+  `tar -cvjSf public/frontend/site.tar.bz2 public/frontend/site`
+else
+  Rails.logger.info "Current frontend content is up to date"
+end
