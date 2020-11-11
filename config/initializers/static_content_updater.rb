4976bbb8d37a1e1b03a9047c7f50c36020003f12
Brandon Sturgeon <brandon@brandonsturgeon.com>
2020-11-11 03:17:02 -0800

    Make content getter clearer

--- config/initializers/static_content_updater.rb
+++ config/initializers/static_content_updater.rb
@@ -1,4 +1,6 @@
-repo_link = "git@github.com:CFC-Servers/cfc_iris_frontend.git"
+repo = "CFC-Servers/cfc_iris_frontend"
+repo_link = "git@github.com:#{repo}.git"
+content_url = "https://github.com/#{repo}/releases/latest/download/built-site.tar.bz2"
 
 current_frontend_version = `cat public/frontend/VERSION`
 latest_frontend_version = `git ls-remote --refs --sort='version:refname' --tags #{repo_link} | cut -d/ -f3-|tail -n1`
@@ -10,8 +12,7 @@ if current_frontend_version != latest_frontend_version
   Rails.logger.info "Current frontend content is out of date, getting the latest..."
   `rm -rf public/frontend/*`
 
-  url = "https://github.com/CFC-Servers/cfc_iris_frontend/releases/latest/download/built-site.tar.bz2"
-  `curl -LO #{url} public/frontend/`
+  `curl -LO #{content_url} public/frontend/`
   `tar -cvjSf public/frontend/site.tar.bz2 public/frontend/site`
 else
   Rails.logger.info "Current frontend content is up to date"
