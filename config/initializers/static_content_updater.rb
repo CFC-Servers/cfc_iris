b7561e181eedfc6b7b2fcda46911b69a4de41602
Brandon Sturgeon <brandon@brandonsturgeon.com>
2020-11-11 03:34:42 -0800

    Fix initializer

--- config/initializers/static_content_updater.rb
+++ config/initializers/static_content_updater.rb
@@ -5,15 +5,16 @@ content_url = "https://github.com/#{repo}/releases/latest/download/built-site.ta
 current_frontend_version = `cat public/frontend/VERSION`
 latest_frontend_version = `git ls-remote --refs --sort='version:refname' --tags #{repo_link} | cut -d/ -f3-|tail -n1`
 
-Rails.logger.debug "Current frontend content version: '#{current_frontend_version}'"
-Rails.logger.debug "Latest frontend content version: '#{latest_frontend_version}'"
+#Rails.logger.debug "Current frontend content version: '#{current_frontend_version}'"
+#Rails.logger.debug "Latest frontend content version: '#{latest_frontend_version}'"
 
 if current_frontend_version != latest_frontend_version
-  Rails.logger.info "Current frontend content is out of date, getting the latest..."
+  #Rails.logger.info "Current frontend content is out of date, getting the latest..."
   `rm -rf public/frontend/*`
 
-  `curl -LO #{content_url} public/frontend/`
-  `tar -cvjSf public/frontend/site.tar.bz2 public/frontend/site`
+  `curl -L #{content_url} -o public/frontend/site.tar.bz2`
+  `tar -C public/frontend/ -xjf public/frontend/site.tar.bz2`
+  `rm public/frontend/site.tar.bz2`
 else
-  Rails.logger.info "Current frontend content is up to date"
+  #Rails.logger.info "Current frontend content is up to date"
 end
