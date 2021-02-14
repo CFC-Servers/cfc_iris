d630736fb6575926a682914ca507792d559a86a5
Brandon Sturgeon <brandon@brandonsturgeon.com>
2021-02-13 22:04:53 -0800

    No more uuid on token

--- app/models/api_token.rb
+++ app/models/api_token.rb
@@ -1,8 +1,4 @@
 class ApiToken < ApplicationRecord
   default_scope { where(active: true) }
   scope :inactive, -> { where(active: false) }
-
-  before_create do
-    self.uuid ||= SecureRandom.uuid
-  end
 end
