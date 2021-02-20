cfdf9f4f34ac53cd5bc14c7e08bcecccbd806503
Brandon Sturgeon <brandon@brandonsturgeon.com>
2021-02-20 12:50:46 -0800

    Unprivate

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,6 @@ class User < ApplicationRecord
   has_many :identities, dependent: :destroy
   has_many :ranks, dependent: :destroy
 
-  private
-
   def consume_users!(*users)
     Rails.logger.log("[User ##{id}] consuming #{users.count} users [#{users.pluck(:id)}]")
     identities = users.map(&:identities)
